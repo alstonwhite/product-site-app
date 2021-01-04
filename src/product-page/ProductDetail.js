9e950c45c6a93275b3194865db246b516061f1e6
alstonwhite <59372363+alstonwhite@users.noreply.github.com>
2021-01-04 08:57:52 -0500

    Added PDP gallery

--- src/product-page/ProductDetail.js
+++ src/product-page/ProductDetail.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import logo from '../logo.svg';
 
 const ProductDetail = ({product, onAdd}) => {
 
+    const [mainImg, setMainImg] = useState(product.image1 ? product.image1.fields.file.url : logo);
+
       return (
         <div className="product-detail" id="product-detail">
-            <img alt='logo' className="product-detail__img" src={product.image1 ? product.image1.fields.file.url : logo}/>
+            <div className='product-detail__img-gallery'>
+                <img alt='logo' className="product-detail__img-main" src={mainImg}/>
+                <img 
+                    alt='logo' 
+                    className="product-detail__img-thumb1" 
+                    src={product.image1 ? product.image1.fields.file.url : logo}
+                    onClick={()=>setMainImg(product.image1 ? product.image1.fields.file.url : logo)}
+                />
+                <img 
+                    alt='logo' 
+                    className="product-detail__img-thumb2" 
+                    src={product.image1 ? product.image2.fields.file.url : logo}
+                    onClick={()=>setMainImg(product.image1 ? product.image2.fields.file.url : logo)}
+                />
+                <img 
+                    alt='logo' 
+                    className="product-detail__img-thumb3" 
+                    src={product.image1 ? product.image3.fields.file.url : logo}
+                    onClick={()=>setMainImg(product.image1 ? product.image3.fields.file.url : logo)}
+                />
+            </div>
             <div className='product-detail__content'>
                 <div className="product-detail__content__name">{product.title}</div>
                 <div className="product-detail__content__price">
